6ff1bed29a49d96a150e5e7f4de2b74307e99fa6
FGMO <fmontalvo8a@hotmail.com>
2018-05-16 10:57:21 -0500

    Inicio del proyecto

---
+++ EasyFood/app/src/main/java/ec/edu/ups/soft/ing/easyfood/MainActivity.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.soft.ing.easyfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
